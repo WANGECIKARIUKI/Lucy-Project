dd2199afd26f3cb821795935dc393c7334aa1538
WANGECIKARIUKI <137193972+WANGECIKARIUKI@users.noreply.github.com>
2023-08-08 20:34:23 +0300

    Final Project

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 function showProducts() {
-  fetch("http://localhost:3000/products")
-    .then(response => response.json())
-    .then(data => createList(data.products))
-    .catch(err => alert(err));
-}
-
-// Create element for products
-function createList(products) {
-  const mainOl = document.createElement('ol');
-
-  for (let i = 0; i < products.length; i++) {
-    const productLi = document.createElement('li');
-    productLi.innerHTML = products[i].name;
-
-    const descriptionLi = document.createElement('li');
-    descriptionLi.innerHTML = 'Description: ' + products[i].description;
-
-    const limitLi = document.createElement('li');
-    limitLi.innerHTML = 'Limit: ' + products[i].limit;
-
-    productLi.appendChild(descriptionLi);
-    productLi.appendChild(limitLi);
-
-    mainOl.appendChild(productLi);
+    fetch("http://localhost:3000/products")
+      .then(response => response.json())
+      .then(data => createList(Products))
+      .catch(err => alert(err));
+  }
+  
+  // Create element for products
+  function createList(products) {
+    const mainOl = document.createElement('ol');
+  
+    for (let i = 0; i < products.length; i++) {
+      const productLi = document.createElement('li');
+      productLi.innerHTML = products[i].name;
+  
+      const descriptionLi = document.createElement('li');
+      descriptionLi.innerHTML = 'Description: ' + products[i].description;
+  
+      const limitLi = document.createElement('li');
+      limitLi.innerHTML = 'Limit: ' + products[i].limit;
+  
+      productLi.appendChild(descriptionLi);
+      productLi.appendChild(limitLi);
+  
+      mainOl.appendChild(productLi);
+    }
+  
+    document.body.appendChild(mainOl);
   }
 
-  document.searchProduct.appendChild(mainOl);
-}
-
-//function to search for products
-  function searchProducts(){
+  //function to search for products
+  function Products(){
   
-    const showProductsUrl = "http://localhost:3000/products"
+    const productsUrl = "https://developer.safaricom.co.ke/APIs"
   
-  return searchProducts;
+  return productsUrl
 
   const productData = {
     method: "POST",
@@ -45,36 +45,39 @@ function createList(products) {
     }
   }
 
-// dom Elements
-const submit = document.getElementById('submit');
-const customerReview = document.getElementById('customerReview');
-const search = document.getElementById("search-products");
+  fetch("http://localhost:3000/products")
+  .then(response => response.json())
+  .then(Data => console.log(Data))
+  .catch(error => {
+      alert("Error Detected!")
+      console.log(err.message)
+  });
 
-// event listeners.
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("DOMContentloaded");
-  showProducts(); // Call the function to load products
-});
 
-submit.addEventListener('click', clickSubmitAlert);
-function clickSubmitAlert() {
-  alert('Submitted!');
-}
-
-customerReview.addEventListener('submit', clickMe);
-function clickMe() {
-  alert('Customer Submit Review!');
-}
-search.addEventListener('click', (e) => {
-    e.preventDefault();
-    productform();
-    submit();
- 
-  } );
-  function searchAlert() {
-    alert('Searched!');
+  // dom Elements
+  const submit = document.getElementById('submit');
+  const CustomerReview = document.getElementById('CustomerReview');
+  const search = document.getElementById("search-products");
+  const productform = document.getElementById("products-form");
+  // event listeners.
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("Products loaded");
+    showProducts(); // Call the function to load products
+  });
+  
+  submit.addEventListener('click', clickSubmitAlert);
+  function clickSubmitAlert() {
+    alert('Submitted!');
   }
-//function to diplay products
-function showProducts() {
-  console.log('products');
-}
+  
+  CustomerReview.addEventListener('click', clickReviewAlert);
+  function clickReviewAlert() {
+    alert('Customer Review Clicked!');
+  }
+
+  search.addEventListener('click', searchAlert);
+  function searchAlert() {
+    alert('Searchd!');
+  }
+    
+    
