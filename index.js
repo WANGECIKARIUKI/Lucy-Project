6d86b0faff61112404fc9674c209481a9b693401
WANGECIKARIUKI <137193972+WANGECIKARIUKI@users.noreply.github.com>
2023-08-08 14:19:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,49 @@
- document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOMContentLoaded");
-  
-  function showProducts() {
-    fetch("http://localhost:3000/products")
-      .then(response => response.json())
-      .then(data => createList(Products))
-      .catch(err => alert(err));
-  }
-  
-  // Create element for products
-  function createList(products) {
-    const mainOl = document.createElement('ol');
-  
-    for (let i = 0; i < products.length; i++) {
-      const productLi = document.createElement('li');
-      productLi.innerHTML = products[i].name;
-  
-      const descriptionLi = document.createElement('li');
-      descriptionLi.innerHTML = 'Description: ' + products[i].description;
-  
-      const limitLi = document.createElement('li');
-      limitLi.innerHTML = 'Limit: ' + products[i].limit;
-  
-      productLi.appendChild(descriptionLi);
-      productLi.appendChild(limitLi);
-  
-      mainOl.appendChild(productLi);
-    }
-  
-    document.searchProduct.appendChild(mainOl);
-  }
-
-  //function to search for products
-  function searchProduct(){
-  
-    const productsUrl = "http://localhost:3000/products"
-  
-  return searchProduct;
-
-  const productData = {
-    method: "POST",
-    headers: {
-        "content-type":"application/json",
-        "Accept":"application/json"
-    },
-    body: JSON.stringify()
-    }
-  }
-
+function showProducts() {
   fetch("http://localhost:3000/products")
-  .then(response => response.json())
-  .then(Data => console.log(Data))
-  .catch(error => {
-      alert("Error Detected!")
-      console.log(err.message)
-  });
-
-
-  // dom Elements
-  const submit = document.getElementById('submit');
-  const CustomerReview = document.getElementById('CustomerReview');
-  const search = document.getElementById("search-products");
-  const searchProduct = document.getElementById("search-product");
-
-  // Event Listener
-  submit.addEventListener('click', clickSubmitAlert);
-  function clickSubmitAlert() {
-    alert('Submitted!');
-  }
-  
-  CustomerReview.addEventListener('submit', clickReviewAlert);
-  function clickReviewAlert() {
-    alert('Customer Review Clicked!');
-  }
-
-  searchProduct.addEventListener('click', (e) => {
-    e.preventDefault();
-    productform();
-    submit();
- 
-  } );
-  function searchAlert() {
-    alert('Searched!');
-  }
-
-//variable to keep track of the search product
-let products = "";
-let keyword = "i";
-
-//function to diplay products
-function searchProduct(){
-  keyword=search.value;
+    .then(response => response.json())
+    .then(data => createList(data.products))
+    .catch(err => alert(err));
 }
-   });
-    
 
+// Create element for products
+function createList(products) {
+  const mainOl = document.createElement('ol');
 
+  for (let i = 0; i < products.length; i++) {
+    const productLi = document.createElement('li');
+    productLi.innerHTML = products[i].name;
 
+    const descriptionLi = document.createElement('li');
+    descriptionLi.innerHTML = 'Description: ' + products[i].description;
 
+    const limitLi = document.createElement('li');
+    limitLi.innerHTML = 'Limit: ' + products[i].limit;
 
+    productLi.appendChild(descriptionLi);
+    productLi.appendChild(limitLi);
+
+    mainOl.appendChild(productLi);
+  }
+
+  document.body.appendChild(mainOl);
+}
+
+// dom Elements
+const submit = document.getElementById('submit');
+const customerReview = document.getElementById('customerReview');
+
+// event listeners.
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Products loaded");
+  showProducts(); // Call the function to load products
+});
+
+submit.addEventListener('click', clickSubmitAlert);
+function clickSubmitAlert() {
+  alert('Submitted!');
+}
+
+customerReview.addEventListener('click', clickReviewAlert);
+function clickReviewAlert() {
+  alert('Customer Review Clicked!');
+}
